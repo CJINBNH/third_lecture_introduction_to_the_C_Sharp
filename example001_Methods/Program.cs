73338f6b8b51faec72eea40b43b398024898486d
CJINBNH <alex_egorov_89@mail.ru>
2022-08-13 20:45:21 +0400

    Renamed folder example001_Methods

---
+++ example001_Methods/Program.cs
@@ -0,0 +1,16 @@
+﻿// 4 вида методов (функций)
+// 1-й вид
+// void Method1();
+// {
+//     System.Console.WriteLine("");
+// }
+// вызвать метод
+// Method1();
+
+// 2-й вид
+
+void Method2(string msg);
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сообщения");
