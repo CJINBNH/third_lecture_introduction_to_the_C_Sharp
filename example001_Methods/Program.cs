cf438ee7cecae7642e15821251cec05736ef9280
CJINBNH <alex_egorov_89@mail.ru>
2022-08-13 21:44:42 +0400

    Добавили в метод цикл for

--- example001_Methods/Program.cs
+++ example001_Methods/Program.cs
@@ -38,16 +38,28 @@
 
 // 4-я группа методов - принимают и возвращают данные
 
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(2, "Нана_на_нана");
+// Console.WriteLine(res);
+
 string Method4(int count, string text)
 {
-    int i = 0;
     string result = String.Empty;
-    while (i < count)
+    for (int i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
     return result;
 }
-string res = Method4(2, "Нана_на_нана");
+string res = Method4(2, "Нана_на_нана Life is Life");
 Console.WriteLine(res);
