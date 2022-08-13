c1cc3820d20f222f32cee9f6eb36ac1c00777611
CJINBNH <alex_egorov_89@mail.ru>
2022-08-13 22:40:35 +0400

    Added an array output method to the console

---
+++ example003/Program.cs
@@ -0,0 +1,14 @@
+﻿// Сортировка массива методом поиска минимального
+
+int[] array = { 9, 8, 3, 7, 9, 1, 6, 2, 5, 4, 3, 2, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
