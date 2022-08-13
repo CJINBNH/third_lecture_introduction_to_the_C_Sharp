1c023d30f61c503bef653fcce22e195f6e7ead22
CJINBNH <alex_egorov_89@mail.ru>
2022-08-13 21:29:56 +0400

    Разобрали 4-е группы методов

--- example001_Methods/Program.cs
+++ example001_Methods/Program.cs
@@ -1,16 +1,53 @@
 ﻿// 4 вида методов (функций)
-// 1-й вид
-// void Method1();
+// 1-я группа методов - ничего не принимает и ничего не возвращает
+
+// void Method1()
 // {
 //     System.Console.WriteLine("");
 // }
 // вызвать метод
 // Method1();
 
-// 2-й вид
+// 2-я группа методов - принимает аргументы, но также ничего не возвращает
 
-void Method2(string msg);
+// void Method2 (string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+//Method2(msg: "Текст сообщения");
+
+// void Method21 (string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         i++;
+//     }
+// }
+// Method21("Текст сообщения", 4);
+// Method21(count: 4, msg: "Текст сообщения");
+
+// 3-я группа методов что-то возвращают, но ничего не принимают 
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3();
+// Console.WriteLine(year);
+
+// 4-я группа методов - принимают и возвращают данные
+
+string Method4(int count, string text)
 {
-    Console.WriteLine(msg);
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
 }
-Method2("Текст сообщения");
+string res = Method4(2, "Нана_на_нана");
+Console.WriteLine(res);
